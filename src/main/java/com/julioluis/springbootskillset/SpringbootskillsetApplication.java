1f1048407537777013158c3633ca24c9cc366cdb
jpelegrino <jpelegrino25@gmail.com>
2020-09-12 09:46:35 -0500

    Set up spring boot web provider for tomcat.

---
+++ src/main/java/com/julioluis/springbootskillset/SpringbootskillsetApplication.java
@@ -0,0 +1,13 @@
+package com.julioluis.springbootskillset;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootskillsetApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootskillsetApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/julioluis/springbootskillset/SpringbootskillsetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.julioluis.springbootskillset;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootskillsetApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
