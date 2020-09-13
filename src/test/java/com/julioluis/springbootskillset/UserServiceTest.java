33bd260c44d62f80e204f0e22a062c33530ea619
jpelegrino <jpelegrino25@gmail.com>
2020-09-13 13:29:16 -0500

    Using Spring test.

---
+++ src/test/java/com/julioluis/springbootskillset/UserServiceTest.java
@@ -0,0 +1,58 @@
+package com.julioluis.springbootskillset;
+
+import com.julioluis.springbootskillset.entities.User;
+import com.julioluis.springbootskillset.repositories.UserRepository;
+import com.julioluis.springbootskillset.services.impl.UserServiceIml;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.jupiter.api.BeforeAll;
+
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.mockito.junit.MockitoJUnitRunner;
+import org.springframework.boot.test.context.SpringBootTest;
+
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+import  static org.mockito.Mockito.*;
+
+@SpringBootTest
+@RunWith(MockitoJUnitRunner.class)
+public class UserServiceTest {
+
+    @Mock
+    private UserRepository userRepository;
+
+    @InjectMocks
+    private UserServiceIml userService;
+
+    @Before
+    public void init() {
+        MockitoAnnotations.initMocks(this);
+    }
+
+
+    @Test
+    public void testFindUserByNameSuccessful() {
+        final String USERNAME="admin";
+        User user=new User();
+        Optional<User> userOptional=Optional.of(user);
+        when(userRepository.findByUsername(anyString())).thenReturn(userOptional);
+        User userFound=userService.findUserByUsername(USERNAME);
+        assertNotNull(userFound);
+    }
+
+    @Test
+    public void testFindUserByNameFail() {
+        final String USERNAME="username";
+        Optional<User> userOptional=Optional.empty();
+        when(userRepository.findByUsername(anyString())).thenReturn(userOptional);
+        User userFound=userService.findUserByUsername(USERNAME);
+        assertNull(userFound);
+    }
+
+}
